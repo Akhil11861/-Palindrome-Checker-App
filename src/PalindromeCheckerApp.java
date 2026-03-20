455870e2b3c90c493d7fe92ea63432aadc539a81
Akhil11861 <mulla.akhilvardhan1186@gmail.com>
2026-03-20 09:13:35 +0530

    UC2

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,6 +1,6 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args){
-        String word = "madam";
+        String word = "racecar";
 
         String reversed = "";
         for (int i = word.length() - 1; i >= 0; i--) {
@@ -9,9 +9,10 @@ public class PalindromeCheckerApp {
 
         if (word.equals(reversed)) {
             System.out.println("Word: " + word);
-            System.out.println("Result: It is a palindrome.");
+            System.out.println("Result: It IS a palindrome.");
         } else {
             System.out.println("Word: " + word);
             System.out.println("Result: It is NOT a palindrome.");
-        }    }
+        }
+    }
 }
